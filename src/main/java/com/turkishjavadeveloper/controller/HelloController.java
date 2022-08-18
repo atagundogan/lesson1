7fd44cccce8ce141466a141795ff84b93b5cdefc
atagundogan <Atagun3566>
2022-08-18 17:29:13 +0300

    GET, SET, PUSH vs gibi işlemler comment olarak eklendi.

--- src/main/java/com/turkishjavadeveloper/controller/HelloController.java
+++ src/main/java/com/turkishjavadeveloper/controller/HelloController.java
@@ -1,17 +1,31 @@
 package com.turkishjavadeveloper.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api")
 public class HelloController {
     // localhost:8081/hello
     // @GetMapping(path = "/hello")
+    // GET Methodu Veri görüntülemek için kullanılır. @GetMapping
+    // POST Methodu Veriyi Kaydetmek için kullanılır. @PostMapping
+    // PUT Methodu Veriyi Güncellemek için kullanılır. @PutMapping
+    // PATCH Methodu Verinin bir bölümünğ güncellemek için kullanılır. örneğin parola! @PatchMapping
+    // DELETE Methodu ise veriyi silmek için kullanılır. @DeleteMapping
+
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String sayHello(){
         return "Hello World!..";
+    }
+    @PostMapping(path = "/save")
+    public String save()
+    {
+        return "Save Data";
+    }
+
+    @DeleteMapping(path="/delete")
+    public String delete()
+    {
+        return "Delete Data";
     }
 }
