57a812edd004f2cb61c3c150d6b57b705cb84e32
atagundogan <Atagun3566>
2022-08-18 16:53:20 +0300

    RequestMapping Anotasyonu

--- src/main/java/com/turkishjavadeveloper/controller/HelloController.java
+++ src/main/java/com/turkishjavadeveloper/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.turkishjavadeveloper.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
     // localhost:8081/hello
-    @GetMapping(path = "/hello")
+    // @GetMapping(path = "/hello")
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String sayHello(){
         return "Hello World!..";
     }
