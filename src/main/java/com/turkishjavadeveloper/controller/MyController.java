8ded9614ec5acea5d4bcf3bd3bc886bb2bd6d939
atagundogan <Atagun3566>
2022-08-25 12:22:05 +0300

    @RequestBody Anotasyonu - tekli ve liste halinde json kayıtlarıyla ilgili çalışma

--- src/main/java/com/turkishjavadeveloper/controller/MyController.java
+++ src/main/java/com/turkishjavadeveloper/controller/MyController.java
@@ -1,6 +1,9 @@
 package com.turkishjavadeveloper.controller;
 
+import com.turkishjavadeveloper.model.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -15,6 +18,27 @@ public String getMyMessage(@RequestParam String message)
     {
         return "Your message: " + message;
     }
+
+
+    /// model içinde oluşturulan user class işlerimlerinden sonra burdan devam ediyoruz.
+    @PostMapping("users")
+    public User saveUser(@RequestBody User user)
+    {
+        System.out.println("User Saved!");
+        user.setPassword("");  // güvenlik gereği password boş bi şekilde görünmesini sağladık.
+        return user;
+    }
+
+    /// üstteki işlemi liste halinde yapmamız için aşağıdaki yol izlendi.
+
+    @PostMapping("users-all")
+    public List<User> saveAllUser(@RequestBody List<User> users)
+    {
+        System.out.println("All Users Saved!");
+        users.forEach(user -> user.setPassword(""));
+        return users;
+    }
+
 //    @GetMapping(path = "/message/{m}")
 //    public String getMyMessage(@PathVariable("m") String message)
 //    {
